d15debb28a5b0f20de22cb10c28f20f6044a5c42
Andrey <melnichenko.andrey@gmail.com>
2018-09-03 15:55:47 +0300

    PIT-144: Reformate code

--- src/main/java/Gmail/MailMainPage.java
+++ src/main/java/Gmail/MailMainPage.java
@@ -1,14 +1,14 @@
 package Gmail;
 
-import com.codeborne.selenide.ElementsCollection;
 import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
 
 public class MailMainPage {
-    public ElementsCollection title (){return $$(By.xpath("//span[@class='bog']"));}
+    public SelenideElement titleInvite (){return $(By.xpath("(//span[@class='bqe' and contains(text(), 'Signup invite')])[2]"));}
+    public SelenideElement titleConfirm (){return $(By.xpath("(//span[@class='bqe' and contains(text(), 'Signup Confirmation')])[2]"));}
+    public SelenideElement titleReset (){return $(By.xpath("(//span[@class='bqe' and contains(text(), 'Password reset for My Application')])[2]"));}
     public SelenideElement checkbox(){return $(By.xpath("//span[@role='checkbox']"));}
     public SelenideElement delteLetters(){return $(By.xpath("//div[@role='button' and @act='10']"));}
 
